38fbd473499871efdaaeb453fd1b96c4ac6d49fa
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2021-12-27 22:21:34 +0530

    npm installed
    installed node module

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+<script>
+import LearningResource from "./components/learning-resources/LearningResource.vue";
+export default {
+  components: { LearningResource },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official vue.js documentation",
+          link: "http://vuejs.org",
+        },
+        {
+          id: "Google",
+          title: "Google",
+          description: "Learn to google",
+          link: "http://google.org",
+        },
+      ],
+    };
+  },
+};
+</script>
